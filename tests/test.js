6bca9752936b12abfb2f960863141b2cd928a82c
Allen Phelps <allenatmc@gmail.com>
2016-01-29 13:27:49 -0600

    tweak test for validate.js

--- tests/test.js
+++ tests/test.js
@@ -124,8 +124,8 @@ describe('cal', () => {
       expect(validateArgs.validate([1600, 3])).to.be.a('null');
       expect(validateArgs.validate([100000, 3])).to.be.a('null');
       expect(validateArgs.validate([1600, 3])).to.be.a('null');
-      expect(validateArgs.validate([1600, 18])).to.be.a('null');
-      expect(validateArgs.validate([1600, 'a'])).to.be.a('null');
+      expect(validateArgs.validate([1800, 18])).to.be.a('null');
+      expect(validateArgs.validate([1900, 'a'])).to.be.a('null');
     });
 
   });
